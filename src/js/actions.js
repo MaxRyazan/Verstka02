2674c4c74ca3ec4cc9b55e2e411a2b1bd5d3e29a
MaxRyazan <no4ta87@mail.ru>
2023-02-08 18:34:38 +0300

    1.1 header done. 100% adaptive

--- src/js/actions.js
+++ src/js/actions.js
@@ -2,20 +2,35 @@ const dropdown = document.querySelector('.header__blog')
 const wrapper = document.querySelector('.wrapper')
 const button = document.querySelector('.dropdown')
 
-
-if(wrapper.clientWidth <= 1280){
-    dropdown.classList.add('hide')
+function hideNav(){
+    if(wrapper.clientWidth <= 1280){
+        dropdown.classList.add('hide')
+    }
 }
-
-window.addEventListener('resize', () => {
+function hideServiceLink(){
+    const service = document.querySelector('.header__service-link')
+    const moveHere  = document.querySelector('.header__blog-links')
+    const link = document.querySelector('.header__blog-child')
+    if(wrapper.clientWidth <= 620){
+        moveHere.appendChild(link)
+    }
+    if(wrapper.clientWidth > 620){
+        service.appendChild(link)
+    }
+}
+function toggleDropdown(){
     if(wrapper.clientWidth <= 1280){
         dropdown.classList.add('hide')
     } else {
         dropdown.classList.remove('hide')
     }
-})
+}
 
-button.addEventListener('click', () => {
-    dropdown.classList.toggle('hide')
-})
+window.addEventListener('resize', toggleDropdown)
 
+window.addEventListener('resize', hideServiceLink)
+
+button.addEventListener('click', () => dropdown.classList.toggle('hide'))
+
+hideServiceLink()
+hideNav()
