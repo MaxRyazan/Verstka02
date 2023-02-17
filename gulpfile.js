f76b79df620ce9fd6aeacabcd2d5cdcf554033f8
MaxRyazan <no4ta87@mail.ru>
2023-02-17 13:17:43 +0300

    1.5 price section done

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,92 @@
-const {src, dest, watch, parallel, series} = require('gulp');
-const scss = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat');
-const browserSync = require('browser-sync').create();
-const uglify = require('gulp-uglify-es').default;
-const autoprefixer = require('gulp-autoprefixer');
+"use strict"
+
+const {src, dest} = require('gulp');
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer'); //выставляет webkit префиксы
+const cssBeautify = require('gulp-cssbeautify');
+const sass = require('gulp-sass')(require('sass'));
+const cssnano = require('gulp-cssnano'); //минимизирует css
+const uglify = require('gulp-uglify'); //минимизирует js в min.js
 const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
 const del = require('del');
+const rigger = require("gulp-rigger");  //собирает js в 1 файл
+const browserSync = require('browser-sync').create();
 
 
-function cleanDist(){
-    return del('dist')
+/* Paths*/
+const srcPath = "src/";
+const distPath = "dist/";
+
+const path = {
+    build: {
+        html:   distPath,
+        css:    distPath + "assets/css/",
+        js:     distPath + "assets/js/",
+        images: distPath + "assets/images/",
+        fonts:  distPath + "assets/fonts/"
+    },
+    src: {
+        html:   srcPath+ "*.html",
+        css:    srcPath + "assets/scss/*.scss",
+        js:     srcPath + "assets/js/*.js",
+        images: srcPath + "assets/images/**/*.{jpeg,jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
+        fonts:  srcPath + "assets/fonts/**/*.{eot,woff,woff2,ttf,svg}",
+    },
+    watch: {
+        html:   srcPath + "**/*.html",
+        css:    srcPath + "assets/scss/**/*.scss",
+        js:     srcPath + "assets/js/**/*.js",
+        images: srcPath + "assets/images/**/*.{jpeg,jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
+        fonts:  srcPath + "assets/fonts/**/*.{eot,woff,woff2,ttf,svg}",
+    },
+    clean: "./" + distPath
+}
+
+function html(){
+    return src(path.src.html, {base: srcPath})
+        .pipe(dest(path.build.html))
+        .pipe(browserSync.reload({stream: true}))
+}
+
+function css(){
+    return src(path.src.css, {base: srcPath + "assets/scss/"})
+        .pipe(sass())
+        .pipe(cssBeautify())
+        .pipe(dest(path.build.css))
+        .pipe(concat('styles.min.css'))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 10 version'],
+            grid: true
+        }))
+        .pipe(dest(path.build.css))
+        .pipe(cssnano({
+            zIndex: false,
+            discardComments: {
+                removeAll: true
+            }
+        }))
+        .pipe(dest(path.build.css))
+        .pipe(browserSync.reload({stream: true}))
+}
+
+function js(){
+    return src(path.src.js, {base: srcPath + "assets/js/"})
+        .pipe(rigger())
+        .pipe(dest(path.build.js))
+        .pipe(concat('javascript.min.js'))
+        .pipe(dest(path.build.js))
+        .pipe(uglify())
+        .pipe(dest(path.build.js))
+        .pipe(browserSync.reload({stream: true}))
 }
 
 function images(){
-    return src('app/images/**/*')
+    return src(path.src.images, {base: srcPath + "assets/images/"})
         .pipe(imagemin(
             [
                 imagemin.gifsicle({ interlaced: true }),
-                imagemin.mozjpeg({ quality: 75, progressive: true }),
+                imagemin.mozjpeg({ quality: 80, progressive: true }),
                 imagemin.optipng({ optimizationLevel: 5 }),
                 imagemin.svgo({
                     plugins: [
@@ -27,63 +96,38 @@ function images(){
                 })
             ]
         ))
-        .pipe(dest('dist/images'))
+        .pipe(dest(path.build.images))
+        .pipe(browserSync.reload({stream: true}))
 }
 
-function bsync () {
+function fonts(){
+    return src(path.src.fonts, {base: srcPath + "assets/fonts/"})
+        .pipe(browserSync.reload({stream: true}))
+}
+
+function clean(){
+    return del(path.clean)
+}
+
+function watchFiles(){
+    gulp.watch([path.watch.html], html)
+    gulp.watch([path.watch.css], css)
+    gulp.watch([path.watch.js], js)
+    gulp.watch([path.watch.images], images)
+    gulp.watch([path.watch.fonts], fonts)
+}
+
+function lifeServer(){
     browserSync.init({
         server: {
-            baseDir: 'app/'
+            baseDir: "./" + distPath
         }
-    });
-}
-
-function scripts(){
-    return src([
-        'node_modules/jquery/dist/jquery.js',
-        'app/js/main.js'
-    ])
-        .pipe(concat('main.min.js'))
-        .pipe(uglify())
-        .pipe(dest('app/js'))
-        .pipe(browserSync.stream())
-}
-
-function preProcessorConverter(){
-    return src('./app/scss/styles.scss')
-        .pipe(scss({outputStyle: 'compressed'}))
-        .pipe(concat('style.min.css'))
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['last 10 version'],
-            grid: true
-        }))
-        .pipe(dest('app/css'))
-        .pipe(browserSync.stream())
-}
-
-function watching(){
-    watch(['app/scss/**/*.scss'], preProcessorConverter);
-    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-    watch(['app/*.html']).on('change', browserSync.reload)
-}
-
-function build(){
-    return src([
-        'app/css/style.min.css',
-        'app/fonts/**/*',
-        'app/js/main.min.js',
-        'app/*.html'
-    ], {base: 'app'})
-        .pipe(dest('dist'))
+    })
 }
 
 
-exports.styles = preProcessorConverter;
-exports.watching = watching;
-exports.bsync = bsync;
-exports.scripts = scripts;
-exports.cleanDist = cleanDist;
-exports.images = images;
+const build = gulp.series(clean, gulp.parallel(html, css, js, images, fonts))
+const watch = gulp.parallel(build, watchFiles, lifeServer)
 
-exports.build = series(cleanDist, images, preProcessorConverter, scripts, build);
-exports.default = parallel(scripts, bsync, watching)
+exports.default = watch;
+
